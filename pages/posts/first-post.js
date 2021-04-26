3ac892aec306dd8bf7bf933bec5a1f7c334eed83
pythonhack76 <lrulvoni@gmail.com>
2021-04-26 17:06:13 +0200

    first commit

---
+++ pages/posts/first-post.js
@@ -0,0 +1,19 @@
+import Link from 'next/link'
+
+export default function FirstPost() { 
+    
+    return ( 
+
+        <>
+<h1>First Post</h1>
+<h2> 
+    <Link href="/">
+        <a>Torna indietro</a>
+</Link>
+</h2>
+</>
+
+
+    ) 
+
+}
